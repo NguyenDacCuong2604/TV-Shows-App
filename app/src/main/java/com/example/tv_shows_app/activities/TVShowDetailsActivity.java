010793bcbf78c4ecc4cf49914caeea3f0598ccdf
NguyenDacCuong2604 <20130215@st.hcmuaf.edu.vn>
2024-09-20 12:37:44 +0700

    setup slider indicator

--- app/src/main/java/com/example/tv_shows_app/activities/TVShowDetailsActivity.java
+++ app/src/main/java/com/example/tv_shows_app/activities/TVShowDetailsActivity.java
@@ -1,13 +1,19 @@
 package com.example.tv_shows_app.activities;
 
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.tv_shows_app.R;
+import com.example.tv_shows_app.adapters.ImageSliderAdapter;
 import com.example.tv_shows_app.databinding.ActivityTvshowDetailsBinding;
 import com.example.tv_shows_app.viewmodels.TVShowDetailsViewModel;
 
@@ -34,7 +40,53 @@ public class TVShowDetailsActivity extends AppCompatActivity {
 
         tvShowDetailsViewModel.getTVShowDetails(tvShowId).observe(this, tvShowDetailsResponse -> {
             activityTvshowDetailsBinding.setIsLoading(false);
-            Toast.makeText(getApplicationContext(), tvShowDetailsResponse.getTvShowDetails().getUrl(), Toast.LENGTH_SHORT).show();
+            if(tvShowDetailsResponse.getTvShowDetails() != null){
+                if(tvShowDetailsResponse.getTvShowDetails().getPictures() != null){
+                    loadImageSlider(tvShowDetailsResponse.getTvShowDetails().getPictures());
+                }
+            }
         });
+    }
+
+    private void loadImageSlider(String[] sliderImages){
+        activityTvshowDetailsBinding.sliderViewPager.setOffscreenPageLimit(1);
+        activityTvshowDetailsBinding.sliderViewPager.setAdapter(new ImageSliderAdapter(sliderImages));
+        activityTvshowDetailsBinding.sliderViewPager.setVisibility(View.VISIBLE);
+        activityTvshowDetailsBinding.viewFadingEdge.setVisibility(View.VISIBLE);
+        setupSliderIndicators(sliderImages.length);
+        activityTvshowDetailsBinding.sliderViewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                setCurrentSliderIndicator(position);
+            }
+        });
+    }
+
+    private void setupSliderIndicators(int count){
+        ImageView[] indicators = new ImageView[count];
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        layoutParams.setMargins(8, 0, 8, 0);
+        for(int i=0; i<indicators.length; i++){
+            indicators[i] = new ImageView(getApplicationContext());
+            indicators[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.background_sider_indicator_inactive));
+            indicators[i].setLayoutParams(layoutParams);
+            activityTvshowDetailsBinding.layoutSliderIndicators.addView(indicators[i]);
+        }
+        activityTvshowDetailsBinding.layoutSliderIndicators.setVisibility(View.VISIBLE);
+        setCurrentSliderIndicator(0);
+    }
+
+    private void setCurrentSliderIndicator(int position){
+        int childCount = activityTvshowDetailsBinding.layoutSliderIndicators.getChildCount();
+        for(int i=0; i<childCount; i++){
+            ImageView imageView = (ImageView) activityTvshowDetailsBinding.layoutSliderIndicators.getChildAt(i);
+            if(i == position){
+                imageView.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.background_slider_indicator_active));
+            }
+            else{
+                imageView.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.background_sider_indicator_inactive));
+            }
+        }
     }
 }
